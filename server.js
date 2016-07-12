ac722bcac44dbf0ffa6159107213556840672013
Vitor Pagan <vitor.pagan@gmail.com>
2016-07-11 23:33:14 -0300

    Configuring package.json and server.js to run on heroku

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
 
 app.get('/stocks/:name', function(req, res){
-	res.send({ name: req.params.name, value: Math.random() * 100});
+  res.send({ name: req.params.name, value: Math.random() * 100});
 });
 
-app.listen(8000);
-console.log('Listening on port 8000...');
+app.listen(port, function() {
+  console.log('Our app is running on http://localhost:' + port);
+});
